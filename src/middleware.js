f96d84fd4554cda210db80061fc9cc269191d251
Kien Do <kiendo95@gmail.com>
2024-03-14 22:21:44 +0700

    Refactor middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,37 +1,38 @@
 const { getUser } = require('./queries');
 
 const withUser = async (req, res, next) => {
-    // Get the user Id from request headers
-    const userId = req.headers['user-id'];
+  // Get the user Id from request headers
+  const userId = req.headers['user-id'];
 
-    if (!userId) {
-        return res.status(403).json({ error: 'Forbidden: You must specify userId' });
-    }
-
-    // Find the user by its ID
-    const user = await getUser(userId);
-
-    if (!user) {
-        return res.status(403).json({ error: 'Forbidden: User not found' });
-    }
-
-    req.user = user;
-      
-    // If the user has the required role, continue to the next middleware or route handler
-    next();
-  };
-
-  // Ensure that a mark is given
-  const withMark = async (req, res, next) => {
-    // Get the mark from request headers
-    const markValue = req.body['mark'];
-
-    if (!markValue) {
-      return res.status(403).json({ error: 'Forbidden: You must specify a mark' });
+  if (!userId) {
+      return res.status(403).json({ error: 'Forbidden: You must specify userId' });
   }
-    req.mark = markValue; 
-    next();
+
+  // Find the user by its ID
+  const user = await getUser(userId);
+
+  if (!user) {
+      return res.status(403).json({ error: 'Forbidden: User not found' });
   }
+
+  req.user = user;
+    
+  // If the user has the required role, continue to the next middleware or route handler
+  next();
+};
+
+// Ensure that a mark is given
+const withMark = async (req, res, next) => {
+  // Get the mark from request headers
+  const markValue = req.body['mark'];
+
+  if (!markValue) {
+    return res.status(400).json({ error: 'Bad Request: You must specify a mark' });
+  }
+
+  req.mark = markValue; 
+  next();
+}
   
 // Middleware to check if the user has the role "student"
 const checkStudentRole = (req, res, next) => {
@@ -75,8 +76,10 @@ const checkTeacherRole = (req, res, next) => {
     next();
   };
   
-exports.withUser = withUser;
-exports.checkAdminRole = checkAdminRole;
-exports.checkTeacherRole = checkTeacherRole;
-exports.checkStudentRole = checkStudentRole;
-exports.withMark = withMark
+modules.export = {
+  withUser,
+  withMark,
+  checkAdminRole,
+  checkTeacherRole,
+  checkStudentRole
+}
